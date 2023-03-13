9fec2f26f79cce62058717b203bedd9e5dae6775
Xelapan <informatica@xelapan.com>
2023-03-13 16:59:32 -0600

    Work routes and view Notfound

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+        publicPath: '/'
     },
     mode: 'development',
     resolve: {
@@ -61,10 +62,6 @@ module.exports = {
         }),
     ],
     devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 3005,
+        historyApiFallback: true,
     }
 };
